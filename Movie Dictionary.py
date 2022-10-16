0cf04362a4e4830ed1fa4d5c2e2425e79f8f682d
kanekiken03 <aratrik.bandyopadhyay.21@aot.edu.in>
2022-10-16 20:50:45 +0530

    Movie Dictionary

---
+++ Movie Dictionary.py
@@ -0,0 +1,50 @@
+MENU_PROMPT="\n Enter 'a' to add a movie, 'l' to see the movies, 'f' to find a movie by title, 'q' to quit: "
+movies = []
+
+def add_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the release year: ")
+
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+         })
+
+def show_movies():
+    for movie in movies:
+        print_movie(movie)
+
+def print_movie(movie):
+    print(f"Title: {movie['title']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release Year: {movie['year']}")
+
+def find_movie():
+    search_title = input("Enter movie title you are looking for: ")
+
+    for movie in movies:
+        if movie["title"] == search_title:
+            print_movie(movie)
+
+user_options = {
+    "a": add_movie,
+    "l": show_movies,
+    "f": find_movie
+    }
+
+
+def menu():
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection in user_options:
+            selected_function = user_options[selection]
+            selected_function()
+        else:
+            print("Unknown Command! Try again!!")
+
+        selection = input(MENU_PROMPT)
+
+
+menu()
