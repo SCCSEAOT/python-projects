14f02531e0859ca031499da773dd439a197d59d4
BISHAL123SEN <97157485+BISHAL123SEN@users.noreply.github.com>
2022-10-31 16:51:24 +0530

    Create BISHAL LP.py

---
+++ BISHAL/BISHAL LP.py
@@ -0,0 +1,19 @@
+
+def checkYear(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+             return True
+    else:
+        return False
+ 
+
+year = 2000
+if(checkYear(year)):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
