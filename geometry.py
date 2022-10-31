b3a5f832cd7857b01e110cda1edb1b9086530967
- <paradact@gmail.com>
2022-10-31 19:35:30 +0100

    Added math functions for calculating areas

---
+++ geometry.py
@@ -0,0 +1,13 @@
+from math import pi
+
+def area_rectangle(x, y):
+    return x*y
+
+def area_square(x):
+    return x*x
+
+def area_triangle(x, y):
+    return x*y*0.5
+
+def area_circle(r):
+    return pi*r*r
